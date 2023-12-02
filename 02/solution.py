11a0d3ba08c352e8e5cdc3a9c1f24fc4608df85a
Sean Egloff <segloff23@gmail.com>
2023-12-02 12:19:52 -0500

    day 02 solve

--- 02/solution.py
+++ 02/solution.py
@@ -1,25 +1,69 @@
+import re
 
 def read():
-    problem = []
+    problem = {}
+    
+    pattern = "Game (?P<id>\d+):(?P<drawString>(;?(,? \d+ [a-z]+)*)*)"
+
+    parseItem = lambda item: (item[1].strip(), int(item[0]))
+
     with open("problem.txt") as problemFile:
-        problem = list(map(int, problemFile.readlines()))
+        for line in problemFile.readlines():
+            matchResult = re.match(pattern, line).groupdict()
+            gameId = int(matchResult["id"])
+            drawSets = [
+                dict(parseItem(item.strip().split(" ")) for item in val.split(","))
+                for val in matchResult["drawString"].split(";")
+            ]
+            problem[int(gameId)] = drawSets
 
     return problem
 
 def partOne(problem):
 
-    print("Part 1: {:d}".format(0))
+    hypothesis = {
+        "red": 12,
+        "green": 13,
+        "blue": 14
+    }
+
+    total = 0
+    for gameId, game in problem.items():
+        invalid = False
+        for draw in game:
+            for color in hypothesis:
+                if color in draw and hypothesis[color] < draw[color]:
+                    invalid = True
+                    break
+            if invalid:
+                break
+        if not invalid:
+            total += gameId
+
+    print("Part 1: {:d}".format(total))
 
 def partTwo(problem):
+    
+    colors = ["red", "blue", "green"]
 
-    print("Part 2: {:d}".format(0))
+    colorMax = lambda color: lambda draw: draw[color] if color in draw else 0
+
+    total = 0
+    for game in problem.values():
+        power = 1
+        for c in colors:
+            power *= max(game, key=colorMax(c))[c]
+        total += power
+
+    print("Part 2: {:d}".format(total))
 
 # %% MAIN CALLS
 if __name__ == "__main__":
 
-    print("Solving Day 2, AoC 2022")
+    print("Solving Day 2, AoC 2023")
 
     problem = read();
 
     partOne(problem);
     partTwo(problem);
+
