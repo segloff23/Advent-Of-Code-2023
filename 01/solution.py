15caaace2ec18f272409ddc1514c5084ae530597
Sean Egloff <segloff23@gmail.com>
2023-12-02 11:43:40 -0500

    day 01 solve

--- 01/solution.py
+++ 01/solution.py
@@ -1,23 +1,37 @@
+import re
 
 def read():
-    problem = []
     with open("problem.txt") as problemFile:
-        problem = list(map(int, problemFile.readlines()))
-
-    return problem
+        return [line.strip() for line in problemFile.readlines()]
 
 def partOne(problem):
 
-    print("Part 1: {:d}".format(0))
+    digits = [re.findall("\d", line) for line in problem];
+    total = sum(int(line[0] + line[-1]) for line in digits);
+
+    print("Part 1: {:d}".format(total))
 
 def partTwo(problem):
+    
+    digitNames = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+    
+    basePattern = "|".join(digitNames)
+    
+    digitLookup = dict((k, str(n + 1)) for n, k in enumerate(digitNames))
 
-    print("Part 2: {:d}".format(0))
+    digitsForward = [re.findall("\d|" + basePattern, line) for line in problem];
+    digitsReverse = [re.findall("\d|" + basePattern[::-1], line[::-1]) for line in problem];
+
+    getDigit = lambda d : digitLookup[d] if d in digitLookup else d
+
+    total = sum(int(getDigit(f[0]) + getDigit(l[0][::-1])) for f, l in zip(digitsForward, digitsReverse));
+
+    print("Part 2: {:d}".format(total))
 
 # %% MAIN CALLS
 if __name__ == "__main__":
 
-    print("Solving Day 1, AoC 2022")
+    print("Solving Day 1, AoC 2023")
 
     problem = read();
 
