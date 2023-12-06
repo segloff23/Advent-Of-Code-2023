15002dc5018bcd240bfc5a3e07f3f273738b5392
Sean Egloff <segloff23@gmail.com>
2023-12-06 14:52:12 -0500

    day 06 solution

--- 06/solution.py
+++ 06/solution.py
@@ -1,23 +1,50 @@
+import re
+import math
 
 def read():
-    problem = []
     with open("problem.txt") as problemFile:
-        problem = list(map(int, problemFile.readlines()))
-
+        timeRaw, distanceRaw = problemFile.readlines()
+        time = [int(x) for x in re.split(" +", timeRaw.split(":")[1].strip())]
+        distance = [int(x) for x in re.split(" +", distanceRaw.split(":")[1].strip())]
+        problem = list(zip(time, distance))
     return problem
+
+def calculateRaceMargin(T, D):
+
+    if not (T**2 > 4*D):
+        return 0
+
+    low = math.ceil((1 / 2) * (T - (T**2 - 4*D)**0.5))
+    high = math.floor((1 / 2) * (T + (T**2 - 4*D)**0.5))
+
+    if not (low * (T - low) > D):
+        low += 1
+    if not (high * (T - high) > D):
+        high -= 1
+
+    return high - low + 1
 
 def partOne(problem):
 
-    print("Part 1: {:d}".format(0))
+    product = 1
+    for T, D in problem:
+        product *= calculateRaceMargin(T, D)
+
+    print("Part 1: {:d}".format(product))
 
 def partTwo(problem):
 
-    print("Part 2: {:d}".format(0))
+    T = int("".join(str(t[0]) for t in problem))
+    D = int("".join(str(d[1]) for d in problem))
+
+    total = calculateRaceMargin(int(T), int(D))
+
+    print("Part 2: {:d}".format(total))
 
 # %% MAIN CALLS
 if __name__ == "__main__":
 
-    print("Solving Day 6, AoC 2022")
+    print("Solving Day 6, AoC 2023")
 
     problem = read();
 
