56f69c817e4eff23782d10e3b1ef9a7e477644d8
Sean Egloff <segloff23@gmail.com>
2023-12-09 00:28:40 -0500

    day 09 solve

--- 09/solution.py
+++ 09/solution.py
@@ -1,25 +1,32 @@
 
 def read():
-    problem = []
     with open("problem.txt") as problemFile:
-        problem = list(map(int, problemFile.readlines()))
+        return [list(map(int, line.split(" "))) for line in problemFile.readlines()]
 
-    return problem
+def partOneTwo(problem):
 
-def partOne(problem):
+    total, total2 = 0, 0
+    for row in problem:
+        N = len(row)
+        i = 1
+        total += row[-1]
+        total2 += i * row[0]
+        while not all(x == 0 for x in row):
+            row = [row[i] - row[i-1] for i in range(1, N)]
+            N -= 1
+            i *= -1
+            total += row[-1]
+            total2 += i * row[0]
 
-    print("Part 1: {:d}".format(0))
+    print("Part 1: {:d}".format(total))
 
-def partTwo(problem):
-
-    print("Part 2: {:d}".format(0))
+    print("Part 2: {:d}".format(total2))
 
 # %% MAIN CALLS
 if __name__ == "__main__":
 
-    print("Solving Day 9, AoC 2022")
+    print("Solving Day 9, AoC 2023")
 
     problem = read();
 
-    partOne(problem);
-    partTwo(problem);
+    partOneTwo(problem);
