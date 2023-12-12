3fb3f003c69f177ed322400bcd703fa7c9338430
Sean Egloff <segloff23@gmail.com>
2023-12-12 12:07:51 -0500

    consolidate total calcs

--- 11/solution.py
+++ 11/solution.py
@@ -8,11 +8,12 @@ def read():
     return (nodes, blankRows, blankCols)
 
 def partOneTwo(problem):
-    
+
     nodes, blankRows, blankCols = problem
     L = len(nodes)
 
-    distances = []
+    total = 0
+    total2 = 0
     for n1 in range(L-1):
         r1, c1 = nodes[n1]
         for n2 in range(n1+1, L):
@@ -20,12 +21,8 @@ def partOneTwo(problem):
             d = abs(r2 - r1) + abs(c2 - c1)
             rowGaps = sum(1 for b in blankRows if (b > r1 and b < r2) or (b > r2 and b < r1))
             colGaps = sum(1 for b in blankCols if (b > c1 and b < c2) or (b > c2 and b < c1))
-            distances.append((d, rowGaps, colGaps))
-
-    weight = lambda d, N: d[0] + (N - 1) * (d[1] + d[2])
-
-    total = sum(weight(d, 2) for d in distances)
-    total2 = sum(weight(d, 1000000) for d in distances)
+            total += d + (rowGaps + colGaps)
+            total2 += d + (1000000 - 1) * (rowGaps + colGaps)
 
     print("Part 1: {:d}".format(total))
     print("Part 2: {:d}".format(total2))
